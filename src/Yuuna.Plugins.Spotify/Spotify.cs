8279e6e6ac569226bda70c2b5e38ebc91e024839
0x0001F36D <viyrex.aka.yuyu@gmail.com>
2020-02-10 19:15:50 +0800

    加入專案檔案。

---
+++ src/Yuuna.Plugins.Spotify/Spotify.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Yuuna.Plugins.Spotify
+{
+    using SpotifyAPI.Web;
+    using SpotifyAPI.Web.Auth;
+    using SpotifyAPI.Web.Enums;
+    using SpotifyAPI.Web.Models;
+
+    using System;
+    using System.Runtime.CompilerServices;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Yuuna.Contracts.Interaction;
+    using Yuuna.Contracts.Modules;
+    using Yuuna.Contracts.Patterns;
+    using Yuuna.Contracts.Semantics;
+
+    [ModuleMetadata("Spotify Web Control Plugin For Yuuna",
+        Author = "Yuuna-Project@Orlys", 
+        Description = "Provides control support for Spotify",
+        Url = "https://github.com/Yuuna-Project/Yuuna-Plugins")]
+    public sealed class Spotify : ModuleBase
+    {
+        private readonly SpotifyWebAPI _api ;
+
+        private readonly AuthorizationCodeAuth _auth; 
+        private Token _token;
+        public Spotify()
+        {
+           this. _api = new SpotifyWebAPI();
+            var url = "http://localhost:4002";
+            var scopes = Scope.PlaylistReadPrivate |
+                        Scope.PlaylistReadCollaborative |
+                        Scope.UserReadPlaybackState |
+                        Scope.AppRemoteControl |
+                        Scope.UserModifyPlaybackState;
+            this._auth = new AuthorizationCodeAuth(
+                Encoding.Unicode.GetString(Convert.FromBase64String("MQAwADcAMgA4AGMAZgA0ADEAZgBhADEANAA3ADMAZgA4AGYANAA3AGMAOAAxADcAZABlAGUAZAAzAGMANQBhAA==")),
+                Encoding.Unicode.GetString(Convert.FromBase64String("OQA4ADIANAAxAGUANABiADYAMwAxAGIANAAyAGEANABhADQAMgA2ADMAYwAwADQAMAAzAGQANwA4ADgAMABiAA==")),
+                url, url, scopes);
+            this._auth.AuthReceived += (sender, payload) => 
+            {
+                var auth = (AuthorizationCodeAuth)sender;
+                auth.Stop();
+                this._token = auth.ExchangeCode(payload.Code).Result;
+            };
+
+            this._auth.Start();
+            this._auth.OpenBrowser(); 
+        }
+
+         
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private async Task CheckToken()
+        { 
+            if (this._token.IsExpired())
+                this._token = await this._auth.RefreshToken(this._token.RefreshToken);
+        }
+
+        protected override void BuildPatterns(IGroupManager g, IPatternBuilder p, dynamic config, dynamic session)
+        {
+            g.Define("pause").AppendOrCreate(new[] { "暫停" });
+            g.Define("resume").AppendOrCreate(new[] { "繼續" });
+            g.Define("play").AppendOrCreate(new[] { "播放" });
+
+            g.Define("next").AppendOrCreate(new[] { "下一首" });
+            g.Define("previous").AppendOrCreate(new[] { "上一首" });
+            g.Define("song").AppendOrCreate(new[] { "歌", "歌曲", "曲目",  "樂曲", "音樂" }); 
+
+            var pause = new Invoke(m =>
+            {
+                this.CheckToken().Wait();
+
+                var er = this._api.PausePlayback();
+                if (er.HasError())
+                {
+                    return (Moods.Sad, er.Error.Message);
+                }
+                return (Moods.Happy, "已暫停播放");
+            });
+
+            p.Build(g["pause"], g["play"]).OnInvoke(pause);
+            p.Build(g["pause"], g["song"]).OnInvoke(pause); 
+
+            var resume = new Invoke(m =>
+            {
+                this.CheckToken().Wait();
+
+                var er = this._api.ResumePlayback(offset: default(int?));
+                if (er.HasError())
+                {
+                    return (Moods.Sad, er.Error.Message);
+                }
+                return (Moods.Happy, "已繼續播放");
+            });
+            p.Build(g["play"], g["song"]).OnInvoke(resume);
+            p.Build(g["resume"], g["play"]).OnInvoke(resume);
+            p.Build(g["resume"], g["play"], g["song"]).OnInvoke(resume);
+
+
+            var next = new Invoke(m =>
+            {
+                this.CheckToken().Wait();
+
+                var er = this._api.SkipPlaybackToNext();
+                if (er.HasError())
+                {
+                    return (Moods.Sad, er.Error.Message);
+                }
+                return (Moods.Happy, "已經切換至下一首囉");
+            });
+            p.Build(g["play"], g["next"]).OnInvoke(next);
+            p.Build(g["play"], g["next"], g["song"]).OnInvoke(next); 
+            p.Build(g["next"], g["song"]).OnInvoke(next); 
+
+
+            var previous = new Invoke(m =>
+            {
+                this.CheckToken().Wait();
+
+                var er = this._api.SkipPlaybackToPrevious();
+                if (er.HasError())
+                {
+                    return (Moods.Sad, er.Error.Message);
+                }
+                return (Moods.Happy, "已經返回到上一首囉");
+            });
+            p.Build(g["play"], g["previous"]).OnInvoke(next);
+            p.Build(g["play"], g["previous"], g["song"]).OnInvoke(next); 
+            p.Build(g["previous"], g["song"]).OnInvoke(previous); 
+        }
+    }
+
+     
+}
